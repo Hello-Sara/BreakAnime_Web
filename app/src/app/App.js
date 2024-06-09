9460eb242bdc8c23e703a14f075cc189acf525cf
Hello-Sara <sara.rodrigues.lina@gmail.com>
2024-06-09 17:11:45 +0200

    create dashboard admin

--- app/src/app/App.js
+++ app/src/app/App.js
@@ -1,7 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import '../stylesheets/Main.css';
+
 import HomePage from '../pages/homepage/HomePage';
 import AdminLogin from '../admin/pages/login/AdminLogin';
+import AdminHome from '../admin/pages/dashboard/AdminHome';
+
 import TagManager from 'react-gtm-module';
 
 
@@ -19,6 +22,7 @@ function App() {
             {/* Autres */}
             <Route path='/' element={<HomePage/>} />
             <Route path='/admin' element={<AdminLogin />} />
+            <Route path='/admin/dashboard' element={<AdminHome />} />
             {/* <Route path='/tarifs' element={<Tarifs />} /> */}
             {/* <Route path='/tarifs' element={<Tarifs />} /> */}
           </Routes>
