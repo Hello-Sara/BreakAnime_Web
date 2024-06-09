52230efae3cd3c0298ccdb57001e606e0ae4bf24
Hello-Sara <sara.rodrigues.lina@gmail.com>
2024-06-09 17:11:41 +0200

    problematic and solution cards

--- app/src/pages/homepage/HomePage.js
+++ app/src/pages/homepage/HomePage.js
@@ -58,7 +58,7 @@ function HomePage() {
             
             <div className="scroll-container">
                 <p className="scroll-text">Scroll</p>
-                <svg width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg className="scroll-arrow" width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M4.49996 6.00075L0.256958 1.75775L1.67196 0.34375L4.49996 3.17275L7.32796 0.34375L8.74296 1.75775L4.49996 6.00075Z" fill="#808080"/>
                 </svg>
             </div>            
@@ -72,22 +72,39 @@ function HomePage() {
                         icon={IconQuizz} 
                         title="Quizz" 
                         text={<>Trouvez des animes qui<br />vous correspondent</>} 
+                        className="card"
                     />
                     <Card 
                         icon={IconSuivi} 
                         title="Suivi Facile" 
                         text={<>Gardez une trace de tout<br />ce que vous regardez</>} 
+                        className="card"
                     />
                     <Card 
                         icon={IconCommunaute} 
                         title="Communaute" 
                         text={<>Laissez des commentaires,<br />notez vos animes préférées</>} 
+                        className="card"
                     />
                     <Card 
                         icon={IconDecouverte} 
                         title="Decouverte" 
                         text={<>Une bibliothèque d'anime,<br />constamment mise à jour</>} 
+                        className="card"
                     />
+                </div>
+            </div>
+
+            <div className="problematic-container">
+                <div className="problematic">
+                    <h2>PROBLEMATIQUE</h2>
+                    <p>La quête pour trouver le prochain chef-d'œuvre peut souvent sembler être un défi de taille. Les amateurs d'anime sont confrontés à une myriade de choix sur les plateformes de streaming, mais la sélection du titre parfait peut s'avérer être un <strong>parcours semé d'embûches.</strong></p>
+                </div>
+                <div className="solution">
+                    <h2>SOLUTION</h2>
+                    <p>
+                    Répondez à un quizz rapide pour définir vos préférences. Break Anime génère alors une sélection personnalisée d'animes correspondant à vos envies, économisant ainsi temps et frustration.
+                    </p>
                 </div>
             </div>
 
