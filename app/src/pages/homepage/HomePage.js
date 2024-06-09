cf378e8e1af0d7b335ead7020e539b6c959043e2
Hello-Sara <sara.rodrigues.lina@gmail.com>
2024-06-09 17:11:40 +0200

    added bubbles with anime image in hero banner

--- app/src/pages/homepage/HomePage.js
+++ app/src/pages/homepage/HomePage.js
@@ -2,11 +2,18 @@ import React from 'react';
 import Menu from '../../components/molecules/menu/Menu';
 import Bouton from '../../components/atoms/bouton/Bouton';
 import Card from '../../components/organisms/cards/Card';
+import Bubble from '../../components/atoms/bubble/Bubble';
 
 import IconQuizz from '../../assets/icons/global/quizz.svg';
 import IconCommunaute from '../../assets/icons/global/communaute.svg';
 import IconDecouverte from '../../assets/icons/global/decouverte.svg';
 import IconSuivi from '../../assets/icons/global/suivi.svg';
+
+import ReZeroImage from '../../assets/img/rezero.png';
+import DemonSlayerImage from '../../assets/img/demon_slayer.png';
+import SpyXImage from '../../assets/img/spyx.png';
+import XImage from '../../assets/img/x.png';
+import MyHeroImage from '../../assets/img/myhero.png';
 
 import './HomePage.css';
 
@@ -16,17 +23,39 @@ function HomePage() {
     return (
         <>
             {/* <Menu /> */}
-            <div className="container">
-                <div className="title-container">
-                    <h1 className="title">Break Anime</h1>
-                    <p >Ne perds plus de temps à chercher quoi regarder,
-                    <br />laisse l'application te proposer des animés
-                    <br />adaptés à tes goûts.</p>
+                    
+            <div className="container hero">                               
+                <div className="bubble-container">
+                    <div className="rezero">
+                        <Bubble src={ReZeroImage} size={"medium"}/>
+                    </div>
+
+                    <div className="demon_slayer">
+                        <Bubble src={DemonSlayerImage} size={"large"}/>
+                    </div>
+                    <div className="spyx">
+                        <Bubble src={SpyXImage} size={"large"}/>
+                    </div>
+                    <div className="x">
+                        <Bubble src={XImage} size={"medium"}/>
+                    </div>
+                    <div className="myhero">
+                        <Bubble src={MyHeroImage} size={"small"}/>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="title-container">
+                        <h1 className="title">Break Anime</h1>
+                        <p >Ne perds plus de temps à chercher quoi regarder,
+                        <br />laisse l'application te proposer des animés
+                        <br />adaptés à tes goûts.</p>
+                    </div>
+                    <div className="button-container">
+                    <Bouton name="Télécharger" onClick={() => console.log('Bouton cliqué')} />
+                </div>
                 </div>
             </div>
-            <div className="button-container">
-                <Bouton name="Télécharger" onClick={() => console.log('Bouton cliqué')} />
-            </div>
+            
             <div className="scroll-container">
                 <p className="scroll-text">Scroll</p>
                 <svg width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg">
