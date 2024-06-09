23e76ad0e3b9facd75c86352ed392020b3da14be
Hello-Sara <sara.rodrigues.lina@gmail.com>
2024-06-09 17:20:08 +0200

    create get anime by genre name

--- api/route/animeRoutes.js
+++ api/route/animeRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get('/animes', animeController.getAllAnimes);
 router.get('/animes/:id', animeController.getAnimeById);
 router.get('/anime/search', animeController.searchAnimeByTitleOrSynonym);
+router.get('/animes/genre/:genreName', animeController.getAnimesByGenreName);
 router.post('/animes', authenticateToken, isAdmin, animeController.createAnime);
 router.put('/animes/:id', authenticateToken, isAdmin, animeController.updateAnime);
 router.delete('/animes/:id', authenticateToken, isAdmin, animeController.deleteAnime);
