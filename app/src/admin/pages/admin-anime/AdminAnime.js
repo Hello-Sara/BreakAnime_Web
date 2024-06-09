4a474d69b27a5d54a15dbf2fdf171b5e205c714b
Hello-Sara <sara.rodrigues.lina@gmail.com>
2024-06-09 17:14:34 +0200

    added popup compoent, tag component, search bar component, anime details popup view

--- app/src/admin/pages/admin-anime/AdminAnime.js
+++ app/src/admin/pages/admin-anime/AdminAnime.js
@@ -1,19 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import AdminMenu from '../../../components/molecules/admin-menu/AdminMenu';
 import './AdminAnime.css';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import Loader from '../../../assets/loaders/search.gif';
+import SecondaryBouton from '../../../components/atoms/secondary-bouton/SecondaryBouton';
+import SearchBar from '../../../components/atoms/search-bar/SearchBar';
+import Popup from '../../../components/molecules/pop-up/Popup';
+import AnimeDetails from '../../popups/anime-details/AnimeDetails';
 
 const AdminAnime = () => {
     const [animes, setAnimes] = useState([]);
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(true);
     const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage, setItemsPerPage] = useState(15);
+    const [itemsPerPage, setItemsPerPage] = useState(6);
+    const [pageMaxOffset, setPageMaxOffset] = useState(10);  // Nombre de pages à afficher autour de la page courante [currentPage
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = animes.slice(indexOfFirstItem, indexOfLastItem);
+    const [filteredAnimes, setFilteredAnimes] = useState([]); 
+    let currentItems = filteredAnimes.slice(indexOfFirstItem, indexOfLastItem);
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const [popupPosition, setPopupPosition] = useState({ top: 0, left: 0 });
+    const spanRef = useRef();
+    const [selectedAnime, setSelectedAnime] = useState({});
+
+    const [openPopupId, setOpenPopupId] = useState(null);
+    const [openDetails, setOpenDetails] = useState(false);
+
+    
 
     useEffect(() => {
         const isConnected = !!localStorage.getItem('token');
@@ -33,6 +48,44 @@ const AdminAnime = () => {
         }
     }, []);
 
+    useEffect(() => {
+        const handleResize = () => {
+            setIsPopupOpen(false);  // Fermer toutes les popups
+            if(window.innerWidth <= 1000) {
+                setItemsPerPage(3);
+                setPageMaxOffset(5);
+            } else if(window.innerWidth > 1000) {
+                setItemsPerPage(6);
+                setPageMaxOffset(10);
+            }
+        };
+    
+        window.addEventListener('resize', handleResize);
+    
+        // Supprimer le gestionnaire d'événements lorsque le composant est démonté
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+
+    useEffect(() => {
+        currentItems = filteredAnimes.slice(indexOfFirstItem, indexOfLastItem);
+        setCurrentPage(1);
+    }, [filteredAnimes]);
+
+
+    const handleSpanClick = (event, anime) => {
+        if (isPopupOpen) {
+            setIsPopupOpen(false);
+        }
+        setSelectedAnime(anime);
+        const popupWidth = window.innerWidth * 0.12;  // Convertir 10vw + 2vw de marge en pixels
+        setPopupPosition({ top: event.clientY, left: event.clientX - popupWidth});
+        setOpenPopupId(anime.id);  // Ouvrir la popup de cet anime     
+        setIsPopupOpen(true); 
+    };
+
     const fetchAnimes = async () => {
         try {
             const response = await axios.get('https://api.breakanime.ninja/api/animes/', {
@@ -41,6 +94,7 @@ const AdminAnime = () => {
                 }
             });
             setAnimes(response.data);
+            setFilteredAnimes(response.data);
             setIsLoading(false);
         } catch (error) {
             console.error(error);
@@ -58,6 +112,13 @@ const AdminAnime = () => {
         } catch (error) {
             console.error(error);
         }
+    };
+
+    const handleShowDetails = (animeId) => {
+        // Handle edit action for the anime with the given ID
+        setIsPopupOpen(false);
+        setOpenDetails(true);
+        console.log('Show details anime:', animeId);
     };
 
     const handleEdit = (animeId) => {
@@ -81,7 +142,7 @@ const AdminAnime = () => {
     };
 
     const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(animes.length / itemsPerPage); i++) {
+    for (let i = 1; i <= Math.ceil(filteredAnimes.length / itemsPerPage); i++) {
         pageNumbers.push(i);
     }
 
@@ -91,13 +152,14 @@ const AdminAnime = () => {
         <AdminMenu></AdminMenu>
         <div className="main-content">
             <h1>Admin Anime</h1>
-            {isLoading ? (
+            { isLoading ? (
                 <div id="loading">
                     <img src={Loader} alt="Loading..." />
                 </div>
                 ) : 
                 (
                     <>
+                    <SearchBar animes={animes} setFilteredAnimes={setFilteredAnimes} />
                     <table>
                         <thead>
                             <tr>
@@ -112,19 +174,46 @@ const AdminAnime = () => {
                                     <td>{anime.titre}</td>
                                     <td>{anime.description}</td>
                                     <td>
-                                        <button onClick={() => handleEdit(anime.id)}>Edit</button>
-                                        <button onClick={() => handleDelete(anime.id)}>Delete</button>
-                                        <button onClick={() => handleAssociateGenre(anime.id)}>Associate Genre</button>
+                                        <span className='actions' ref={spanRef} onClick={(event) => handleSpanClick(event, anime)}>...</span>
                                     </td>
+                                    <Popup isOpen={isPopupOpen && openPopupId === anime.id} top={popupPosition.top} left={popupPosition.left} onClose={() => setIsPopupOpen(false)}>                                                           
+                                        <SecondaryBouton 
+                                            Submit={() => selectedAnime && handleShowDetails(selectedAnime.titre)}                                                 
+                                            name={"Show details"} 
+                                            style={{ margin: '10px', padding: '6px 6px 6px 6px', "backgroundColor": '#fff', 'border': '2px solid #FEC200', 'borderRadius': '5px', 'backdropFilter': 'blur(40px)', cursor: 'pointer'}}
+                                            onHoverStyle={{ margin: '10px', padding: '6px 6px 6px 6px', "backgroundColor": '#FEC200', 'border': '2px solid transparent', 'borderRadius': '5px', 'backdropFilter': 'blur(40px)', cursor: 'pointer'}}
+                                        />
+                                        <SecondaryBouton 
+                                            Submit={() => selectedAnime && handleEdit(selectedAnime.titre)}                                                 
+                                            name={"Edit"} 
+                                            style={{ margin: '10px', padding: '6px 6px 6px 6px', "backgroundColor": '#fff', 'border': '2px solid #494946', 'borderRadius': '5px', 'backdropFilter': 'blur(40px)', cursor: 'pointer'}}
+                                            onHoverStyle={{ margin: '10px', padding: '6px 6px 6px 6px', 'color': '#fff', "backgroundColor": '#494946', 'border': '2px solid transparent', 'borderRadius': '5px', 'backdropFilter': 'blur(40px)', cursor: 'pointer'}}
+                                        />
+                                        <SecondaryBouton 
+                                            Submit={() => selectedAnime && handleAssociateGenre(selectedAnime.id)}                                                 
+                                            name={"Associate Genre"} 
+                                            style={{margin: '10px', padding: '6px 6px 6px 6px', "backgroundColor": '#fff', 'border': '2px solid #494946', 'borderRadius': '5px', 'backdropFilter': 'blur(40px)', cursor: 'pointer'}}
+                                            onHoverStyle={{ margin: '10px', padding: '6px 6px 6px 6px','color': '#fff', "backgroundColor": '#494946', 'border': '2px solid transparent', 'borderRadius': '5px', 'backdropFilter': 'blur(40px)', cursor: 'pointer'}}
+                                        />
+                                        <SecondaryBouton 
+                                            Submit={() => selectedAnime && handleDelete(selectedAnime.id)}                                                 
+                                            name={"Delete"} 
+                                            style={{margin: '10px', padding: '6px 6px 6px 6px', "backgroundColor": '#fff', 'border': '2px solid #F25252', 'borderRadius': '5px', 'backdropFilter': 'blur(40px)', cursor: 'pointer' }}
+                                            onHoverStyle={{ margin: '10px', padding: '6px 6px 6px 6px', 'color': '#fff', "backgroundColor": '#F25252', 'border': '2px solid transparent', 'borderRadius': '5px', 'backdropFilter': 'blur(40px)', cursor: 'pointer'}}
+                                        />
+                                    </Popup>
                                 </tr>
                             ))}
+                            <Popup isOpen={openDetails} onClose={() => setOpenDetails(false)} centered={true} size='xxl'>
+                               <AnimeDetails anime={selectedAnime} />
+                            </Popup>
                         </tbody>                    
                     </table>
                     <ul id="page-numbers">
                         {pageNumbers.map(number => {
-                            if (number < currentPage + 10 && number > currentPage - 10) {
+                            if (number < currentPage + pageMaxOffset && number > currentPage - pageMaxOffset) {
                                 return (
-                                    <li key={number} id={number} onClick={handlePageChange} style={{ cursor: 'pointer' }}>
+                                    <li key={number} id={number} onClick={handlePageChange} style={{ cursor: 'pointer' }} className={number === currentPage ? 'current-page' : 'page-number'}>
                                     {number}
                                  </li>
                                 )
